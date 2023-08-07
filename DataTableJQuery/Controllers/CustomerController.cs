8e239736caa9a5e475e97e283482ab89096f4432
Md Asif Alam Alam <measifalam@gmail.com>
2023-08-08 02:28:24 +0530

    ServerSide

--- DataTableJQuery/Controllers/CustomerController.cs
+++ DataTableJQuery/Controllers/CustomerController.cs
@@ -1,6 +1,9 @@
 ﻿using DataTableJQuery.Data;
 using DataTableJQuery.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Web;
+using System.Linq.Dynamic.Core;
 
 namespace DataTableJQuery.Controllers
 {
@@ -21,12 +24,43 @@ namespace DataTableJQuery.Controllers
             return View(customerData);
             //return new JsonResult(customerData);
         }
+
+        [HttpPost]
         public IActionResult GetJson()
         {
-            List<Customer> customerData = _db.Customers.ToList();
-             return Json(new { data = customerData });
-            // return View(customerData);
-           // return new JsonResult(customerData);
+            try
+            {
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+                var customerData = (from tempcustomer in _db.Customers select tempcustomer);
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                {
+                    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    customerData = customerData.Where(m => m.FirstName.Contains(searchValue)
+                                                || m.LastName.Contains(searchValue)
+                                                || m.Contact.Contains(searchValue)
+                                                || m.Email.Contains(searchValue));
+                }
+                recordsTotal = customerData.Count();
+                var data = customerData.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+                return Ok(jsonData);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
-}
+    }
+
--- DataTableJQuery/wwwroot/js/customer.js
+++ DataTableJQuery/wwwroot/js/customer.js
@@ -8,17 +8,25 @@ function loadDataTable() {
         "ajax": {
             url: '/Customer/GetJson',
             dataSrc: 'data',
+            type:"POST"
+           
 }, 
         
         "columns": [
            
-            { data: 'firstName' },
-            { data: 'lastName' },
-            { data: 'contact' },
-            { data: 'email' },
-            { data: 'dateOfBirth' }
+            { "data": 'firstName', name:"firstName" },
+            { "data": 'lastName', name: "lastName" },
+            { "data": 'contact', name: "contact" },
+            { "data": 'email', name: "email" },
+            { "data": 'dateOfBirth', name: "dateOfBirth" }
            
-        ]
+        ],
+        "serverSide": "true",
+        "order": [0, "asc"],
+        "processing": "true",
+        "language": {
+            "processing": "Loading dude"
+        }
     });
     
 }
